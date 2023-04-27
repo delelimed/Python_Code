22d45a1da73d0e214c14c5464e8e628eccc0ab13
Elia <91612533+devdeleli@users.noreply.github.com>
2023-04-27 15:13:09 +0200

    Update paperino.py

--- 26Aprile - PrimaLezione/paperino.py
+++ 26Aprile - PrimaLezione/paperino.py
@@ -1,15 +1,11 @@
-def main():
+
 name = str(input("Come ti chiami? ").lower())
 if name == "anna gaia":
-  print("nduja")
-  
+    print("nduja")
+ 
 elif name == "elia":
-  print("ragazzo fantastico e tutto")
-  
+ print("ragazzo fantastico e tutto")
+ 
 else:
   print("Ciao " + name + ", non so chi sei")
-
-if __name__ == '__main__':
-    main()
-
 
