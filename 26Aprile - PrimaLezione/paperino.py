745feb95a098c9a07e5a5f8179ed04172f0eabcb
Elia <91612533+devdeleli@users.noreply.github.com>
2023-04-27 11:02:24 +0200

    Add files via upload

---
+++ 26Aprile - PrimaLezione/paperino.py
@@ -0,0 +1,9 @@
+name = str(input("Come cabbo ti chiami? "))
+if name == "Anna Gaia":
+  print("nduja")
+  
+elif name == "Elia":
+  print("ragazzo fantastico e tutto")
+  
+else:
+  print("Ciao " + name + ", non so chi sei")
