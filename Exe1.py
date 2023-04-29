10b85153055285337eb263ca1850006bf04c7774
Elia <91612533+devdeleli@users.noreply.github.com>
2023-04-29 22:01:32 +0200

    Create Exe1.py
    Esercizio 1 della seconda lezione

---
+++ Exe1.py
@@ -0,0 +1,16 @@
+'''
+Il presente programma chiede in input la lunghezza della lista. In seguito, chiede i singoli elementi della lista, per poi stamparla a schermo, 
+invertirla e stamparla nuovamente a schermo (invertita).
+'''
+
+NItm = int(input("Da quanti elementi è composta la lista? "))
+ItmList = []
+
+for i in range(NItm):
+    ITM = int(input("Inserisci il numero: "))
+    ItmList.append(ITM)
+print("Hai fornito la seguente lista:")
+print(ItmList)
+print("Di seguito trovi la lista inversa: ")
+ItmList = ItmList[NItm::-1]
+print(ItmList)
