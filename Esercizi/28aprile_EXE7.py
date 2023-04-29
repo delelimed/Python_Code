fcc835ce2927b01aaef4e30fd29c8552ae876700
elia-dr <delrossoelia@gmail.com>
2023-04-30 00:45:41 +0200

    in corso esercizio 7

---
+++ Esercizi/28aprile_EXE7.py
@@ -0,0 +1,32 @@
+'''
+Questo programma prende un numero in ingresso, chiede la base di partenza e lo converte in una base di arrivo.
+'''
+
+PRENumero = str(input("Inserisci il numero che vuoi convertire: "))
+Numero = []
+B1 = int(input("Inserisci la base di partenza: "))
+B2 = int(input("Inserisci la base di arrivo: "))
+Finale = 0
+VOID = None
+
+#inizio la divisione del PRENumero nel Numero (LISTA)
+tempy = 0
+for i in range(len(PRENumero)):
+    Numero.append(PRENumero[tempy])
+    tempy += 1
+tempy = 0
+#Verifica della leicità dell'operazione
+
+if any(int(num) > B1 for num in Numero):
+    VOID = True
+else:
+    VOID = False
+
+if VOID == True:
+    print("Attenzione! Il numero inserito o la base di partenza sono errate!")
+    quit()
+else:
+    print("Poi vediamo")
+
+
+print("Il numero " + str(PRENumero) + " in base " + str(B1) + ", convertito in base " + str(B2) + ", è: " + str(Finale))
