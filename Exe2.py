2fed90c663f3c3e9a95ecb81dc477dbdf09f7aad
Elia <91612533+devdeleli@users.noreply.github.com>
2023-04-29 22:11:34 +0200

    Create Exe2.py
    Esercizio 2 della seconda lezione

---
+++ Exe2.py
@@ -0,0 +1,19 @@
+'''
+Questo programma chiede in input il numero di elementi di una lista, seguito dalla lista stessa. Poi, chiede di inserire un numero, per verificare che esso sia presente.
+'''
+
+NItm = int(input("Da quanti elementi è composta la lista? "))
+ItmList = []
+
+for i in range(NItm):
+    ITM = int(input("Inserisci il numero: "))
+    ItmList.append(ITM)
+print("Hai fornito la seguente lista:")
+print(ItmList)
+
+Verifica = int(input("Di quale valore vuoi verificare la presenza nella lista? "))
+
+if Verifica in ItmList:
+    print("Il valore " + str(Verifica) + " è presente nella lista")
+else:
+    print("Il valore " + str(Verifica) + " NON è presente nella lista")
