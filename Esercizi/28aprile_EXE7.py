323f38494d8ef68260b31d70f4c3d91447cde604
elia-dr <delrossoelia@gmail.com>
2023-04-30 15:41:30 +0200

    in corso esercizio 7

--- Esercizi/28aprile_EXE7.py
+++ Esercizi/28aprile_EXE7.py
@@ -1,23 +1,26 @@
 '''
 Questo programma prende un numero in ingresso, chiede la base di partenza e lo converte in una base di arrivo.
+Come prima cosa converte il numero come intermezzo binario, poi lo converte nella base di destinazione.
 '''
 
-PRENumero = str(input("Inserisci il numero che vuoi convertire: "))
-Numero = []
+Numero1 = str(input("Inserisci il numero INTERO che vuoi convertire: "))
+Numero1lista = []
+Binstring = []
+
 B1 = int(input("Inserisci la base di partenza: "))
 B2 = int(input("Inserisci la base di arrivo: "))
 Finale = 0
 VOID = None
 
-#inizio la divisione del PRENumero nel Numero (LISTA)
+#inizio la divisione del PRENumero1 nel Numero (LISTA)
 tempy = 0
-for i in range(len(PRENumero)):
-    Numero.append(PRENumero[tempy])
+for i in range(len(Numero1)):
+    Numero1lista.append(Numero1[tempy])
     tempy += 1
-tempy = 0
+tempy = 1
+Numero1 == int(Numero1)
 #Verifica della leicità dell'operazione
-
-if any(int(num) > B1 - 1 for num in Numero):
+if any(int(num) > B1 - 1 for num in Numero1lista):
     VOID = True
 else:
     VOID = False
@@ -25,8 +28,3 @@ else:
 if VOID == True:
     print("Attenzione! Il numero inserito o la base di partenza sono errate!")
     quit()
-else:
-    print("Poi vediamo")
-
-
-print("Il numero " + str(PRENumero) + " in base " + str(B1) + ", convertito in base " + str(B2) + ", è: " + str(Finale))
